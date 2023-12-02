6cb00dfa3fc6315152a8255995120d36fd084e5c
youngbloood <944168348@qq.com>
2023-12-02 22:20:38 +0800

    feat: rsbit

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+// The Operation of bit
+pub trait BitOperation {
+    /// Set the pos bit value to 0;
+    /// The pos must be between 0 to 7;
+    /// # Examples
+    ///
+    /// 1010_1000
+    ///
+    /// If the pos is 1, self will become 1010_1010.
+    fn set_0(&mut self, pos: u8);
+
+    /// Set the pos bit value to 1;
+    /// The pos must be between 0 to 7;
+    /// # Examples
+    ///
+    /// 1010_1000
+    ///
+    /// If the pos is 1, self will become 1010_1010.
+    fn set_1(&mut self, pos: u8);
+
+    /// Set the pos bit value to the inverse value(0->1,1->0);
+    /// The pos must be between 0 to 7;
+    /// # Examples
+    ///
+    /// 1010_1010
+    ///
+    /// If the pos is 1, self will become 0b1010_1000.
+    ///
+    /// If the pos is 4, self will become 0b1011_1010 (base on the original 0b1010_1010).
+    fn set_inverse(&mut self, pos: u8);
+}
+
+impl BitOperation for &mut u8 {
+    fn set_0(&mut self, pos: u8) {
+        if pos >= 8 {
+            return;
+        }
+        let flag = 1 << pos;
+        // pos位是0，直接返回
+        if flag != **self & flag {
+            return;
+        }
+        // pos位是1，将其置为0
+        **self = **self & (!flag);
+    }
+
+    fn set_1(&mut self, pos: u8) {
+        if pos >= 8 {
+            return;
+        }
+        let flag = 1 << pos;
+        // pos位是1，直接返回
+        if flag == **self & flag {
+            return;
+        }
+        // pos位是0，将其置为1
+        **self = **self & u8::MAX | flag;
+    }
+
+    fn set_inverse(&mut self, pos: u8) {
+        if pos >= 8 {
+            return;
+        }
+        let flag = 1 << pos;
+        // pos位为1，将其置为0
+        if flag == **self & flag {
+            // pos位是1，将其置为0
+            **self = **self & (!flag);
+            return;
+        }
+        // pos位是0，将其置为1
+        **self = **self & u8::MAX | flag;
+    }
+}
---
+++ tests/lib.rs
@@ -0,0 +1,48 @@
+use rsbit::BitOperation;
+#[test]
+pub fn test_u8_set_0() {
+    let mut src = 0b1000_1010 as u8;
+    (&mut src).set_0(0);
+    assert_eq!(138 as u8, src);
+
+    (&mut src).set_0(1);
+    assert_eq!(136 as u8, src);
+
+    (&mut src).set_0(7);
+    assert_eq!(8 as u8, src);
+
+    (&mut src).set_0(8);
+    assert_eq!(8 as u8, src);
+}
+
+#[test]
+pub fn test_u8_set_1() {
+    let mut src = 0b0000_0010 as u8;
+    (&mut src).set_1(1);
+    assert_eq!(2 as u8, src);
+
+    (&mut src).set_1(0);
+    assert_eq!(3 as u8, src);
+
+    (&mut src).set_1(7);
+    assert_eq!(131 as u8, src);
+
+    (&mut src).set_1(8);
+    assert_eq!(131 as u8, src);
+}
+
+#[test]
+pub fn test_u8_set_inverse() {
+    let mut src = 0b1000_1010 as u8;
+    (&mut src).set_inverse(0);
+    assert_eq!(139 as u8, src);
+
+    (&mut src).set_inverse(1);
+    assert_eq!(137 as u8, src);
+
+    (&mut src).set_inverse(7);
+    assert_eq!(9 as u8, src);
+
+    (&mut src).set_inverse(8);
+    assert_eq!(9 as u8, src);
+}
